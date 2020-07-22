16c986b1071ef65d2d39eb40ea352c2fec949a52
Martin Streile <martin.streile@etecture.de>
2020-07-22 18:06:20 +0200

    Set up premium project.

---
+++ rumspielereien/premium-todo-app/src/Components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TopBar } from "../TopBar/TopBar";
+
+function App() {
+  return (
+    <main className="App">
+      <TopBar />
+    </main>
+  );
+}
+
+export default App;
---
+++ rumspielereien/premium-todo-app/src/Components/TopBar/TopBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TodoInputField } from "../TodoInputField/TodoInputField";
+
+export const TopBar = () => {
+  return (
+    <div>
+      <TodoInputField />
+    </div>
+  );
+};
